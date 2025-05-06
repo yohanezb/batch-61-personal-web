8a64345b5aa196026bbea930c958e6e0462fcb4d
Mayatic <yohanez.b@gmail.com>
2025-05-07 06:40:10 +0700

    task 5

---
+++ contact.js
@@ -0,0 +1,19 @@
+// contact.js
+const form = document.getElementById("form");
+
+form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phoneNumber = document.getElementById("phoneNumber").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    console.log("Form Submitted:");
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Phone Number:", phoneNumber);
+    console.log("Subject:", subject);
+    console.log("Message:", message);
+});
